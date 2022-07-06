43793a3ac281964900acd626a4372c27094985fa
victorleon0 <victor.leon.huerta@gmail.com>
2022-07-06 18:10:09 +0100

    FEAT: add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import './App.scss';
-import Navigation from './components/Navigation/Navigation.jsx'
-
+import "./App.scss";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div className="App">
-    <div className="navigator">
-    <Navigation />
-    </div>
-    
-
-
-    
-    
-  
-    
+      <div className="navigator">
+        <Navigation />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
