37adaa2245e7650b5439bbe9e878f69d1735969c
victorleon0 <victor.leon.huerta@gmail.com>
2022-07-11 15:58:58 +0100

    Fix: Changes

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,22 +2,34 @@ import React from "react";
 import { useState } from "react";
 import "./Login.scss";
 import Navigation from "../../components/Navigation/Navigation";
+import { useNavigate } from "react-router-dom";
 
-const Login = (prop) => {
+const Login = () => {
+  const navigate = useNavigate()
   const [form, setForm] = useState({ email: "", password: "" });
 
   const submitLogin = (ev) => {
     ev.preventDefault();
-    prop.loginUser(form);
+    fetch('http://localhost:6022/users/login', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(form), // data can be string or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    }).then(res => res.json())
+    .then(res => {console.log('Success:', res); alert('Login Correcto'); navigate('/user') })
+
+    .catch(error => console.error('Error:', error))
   };
 
   const handleInput = (ev) =>
     setForm({ ...form, [ev.target.name]: ev.target.value });
 
   return (
-    <div class="login">
+    <div className="login">
       <div className="navigator">
-        <Navigation />
+        {/* <Navigation /> */}
       </div>
       <div className="formContainer">
         <h4>Login</h4>
@@ -25,14 +37,14 @@ const Login = (prop) => {
           <input
             type="text"
             name="username"
-            class="username form-control"
+            className="username form-control"
             placeholder="Username"
             onChange={handleInput}
           />
           <input
             type="password"
             name="password"
-            class="password form-control"
+            className="password form-control"
             placeholder="Password"
             onChange={handleInput}
           />
@@ -44,7 +56,7 @@ const Login = (prop) => {
           >
             Login
           </button>
-          <a href="/users/register">¿No tienes cuenta? Regístrate</a>
+          <p>¿No tienes cuenta? Regístrate</p>
         </form>
       </div>
     </div>
