8a17ec84835466fe34b2e53b93c7466c0d7394e5
Perillos <valero.ce@gmail.com>
2022-07-11 12:08:46 +0200

    login cookies

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import { useState } from "react";
 import "./Login.scss";
 import Navigation from "../../components/Navigation/Navigation";
+import { useNavigate } from "react-router-dom";
 
-const Login = (prop) => {
+const Login = () => {
+  const navigate = useNavigate()
   const [form, setForm] = useState({ email: "", password: "" });
 
   const submitLogin = (ev) => {
     ev.preventDefault();
-    prop.loginUser(form);
+    fetch('http://localhost:6022/users/login', {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(form), // data can be `string` or {object}!
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    }).then(res => res.json(), alert('Login Correcto'), navigate('/trips'))
+    .catch(error => console.error('Error:', error))
   };
 
   const handleInput = (ev) =>
     setForm({ ...form, [ev.target.name]: ev.target.value });
 
   return (
-    <div class="login">
+    <div className="login">
       <div className="navigator">
-        <Navigation />
+        {/* <Navigation /> */}
       </div>
       <div className="formContainer">
         <h4>Login</h4>
@@ -25,14 +35,14 @@ const Login = (prop) => {
           <input
             type="text"
             name="username"
-            class="username form-control"
+            className="username form-control"
             placeholder="Username"
             onChange={handleInput}
           />
           <input
             type="password"
             name="password"
-            class="password form-control"
+            className="password form-control"
             placeholder="Password"
             onChange={handleInput}
           />
