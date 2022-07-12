4ac7f19bfac7642f360e4131fe72dd49987694b9
victorleon0 <victor.leon.huerta@gmail.com>
2022-07-12 09:12:59 +0100

    FEAT: EXCURSIONES PINTADAS

--- src/components/Experiences/ExperiencesAll.jsx
+++ src/components/Experiences/ExperiencesAll.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+import './ExperiencesAll.scss';
+
 const ExperiencesAll = () => {
   const [experiences, setExperiences] = useState([]);
   useEffect(() => {
@@ -16,30 +18,39 @@ const ExperiencesAll = () => {
     };
 
     getExperiences();
-  }, []) ; 
- 
+  }, []);
+
   return (
     <>
-      <h1>Excursiones</h1>
+      <h4>Reserva ahora tu experiencia y disfruta de un descuento único</h4>
       <div className="experiences__container">
-        {experiences && experiences.length > 0 && experiences.map((experience) => {
-          return (
-            <div className="experience" key={experience.id}>
-              <img className="experience__image" src={experience.imagen} alt={experience.titulo} />
-              <div>
-              Price: {experience.precio}€
+        {experiences &&
+          experiences.length > 0 &&
+          experiences.map((experience) => {
+            return (
+              <div className="experience" key={experience.id}>
+                <img
+                  className="image"
+                  src={experience.imagen}
+                  alt={experience.titulo}
+                />
+                <div className="precio">Price: {experience.precio}€</div>
+                <div className="title">
+                  Experience: <strong>{experience.titulo}</strong>
+                </div>
+                <div className="description">
+                  Descripción: <strong>{experience.subtitulo}</strong>
+                </div>
+                <div className="language">
+                  Lang: <strong>{experience.idioma}</strong>
+                </div>
+                <a href="/login">
+                  <button>¡Reserva ahora!</button>
+                </a>
               </div>
-              <div>
-                Experience: <strong>{experience.titulo}</strong>
-              </div>
-              <div>
-                Descripción: <strong>{experience.subtitulo}</strong>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
-      
     </>
   );
 };
