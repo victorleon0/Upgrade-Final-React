c17687d1c4823b8c700c5e724ef7a7ff04886ab0
Perillos <valero.ce@gmail.com>
2022-07-15 10:49:41 +0200

    detodo

---
+++ src/redux/experience/experience.actions 2.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { experienceUrl } from "../../helpers/url.helper";
+export const GET_ALL_EXPERIENCES = "GET_ALL_EXPERIENCES";
+export const ADD_EXPERIENCE = "ADD_EXPERIENCE";
+export const DELETE_EXPERIENCE = "DELETE_EXPERIENCE";
+export const EDIT_EXPERIENCE = "EDIT_EXPERIENCE";
+export const CREATE_EXPERIENCE = "CREATE_EXPERIENCE";
+
+export const getAllExperiences = () => async dispatch => {
+  const res = await axios(experienceUrl);
+  dispatch({ type: GET_ALL_EXPERIENCES, payload: res.data });
+}
+
+export const addExperience = (data) => async (dispatch) => {
+  const res = await axios.post(experienceUrl, data);
+  const experience = res.data;
+  dispatch({
+    type: ADD_EXPERIENCE,
+    payload: experience,
+  });
+};
+
+export const deleteExperience = (experience) => async (dispatch) => {
+  const res = await axios.delete(`${experienceUrl}/${experience._id}`, experience);
+  dispatch({ 
+    type: DELETE_EXPERIENCE, 
+    payload: res.data 
+  });
+  
+
+};
+
+
+export const editExperience = (editExperience, id) => async (dispatch) => {
+  const res = await axios.put(`${experienceUrl}/${id}`, editExperience);
+  dispatch({
+    type: EDIT_EXPERIENCE,
+    payload: {editExperience, res},
+  });
+
+};
+
+export const createExperience = (data) => async (dispatch) => {
+  const res = await axios.post(experienceUrl, data);
+  
+  dispatch({
+    type: CREATE_EXPERIENCE,
+    payload: res.data,
+  });
+};
